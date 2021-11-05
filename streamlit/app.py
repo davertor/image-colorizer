ee49176c6c557e7959e4bffb85c2cbf4573d73c4
Daniel Verdu <dlvs@gft.com>
2021-11-05 09:22:28 +0100

    first commit

---
+++ streamlit/app.py
@@ -0,0 +1,243 @@
+# Import general purpose libraries
+import os, sys, re
+import streamlit as st
+import PIL
+from PIL import Image
+import cv2
+import numpy as np
+import uuid
+from zipfile import ZipFile, ZIP_DEFLATED
+from io import BytesIO
+
+# Import util functions from deoldify
+# NOTE:  This must be the first call in order to work properly!
+from deoldify import device
+from deoldify.device_id import DeviceId
+#choices:  CPU, GPU0...GPU7
+device.set(device=DeviceId.CPU)
+from deoldify.visualize import *
+
+# Import util functions from app_utils
+from app_utils import get_model_bin
+
+
+
+####### INPUT PARAMS ###########
+model_folder = 'models/'
+max_img_size = 800
+################################
+
+@st.cache(allow_output_mutation=True, show_spinner=False)
+def load_model(model_dir, option):
+    if option.lower() == 'artistic':
+        model_url = 'https://data.deepai.org/deoldify/ColorizeArtistic_gen.pth'
+        get_model_bin(model_url, os.path.join(model_dir, "ColorizeArtistic_gen.pth"))
+        colorizer = get_image_colorizer(artistic=True)
+    elif option.lower() == 'stable':
+        model_url = "https://www.dropbox.com/s/usf7uifrctqw9rl/ColorizeStable_gen.pth?dl=0"
+        get_model_bin(model_url, os.path.join(model_dir, "ColorizeStable_gen.pth"))
+        colorizer = get_image_colorizer(artistic=False)
+
+    return colorizer
+
+def resize_img(input_img, max_size):
+    img = input_img.copy()
+    img_height, img_width = img.shape[0],img.shape[1]
+
+    if max(img_height, img_width) > max_size:
+        if img_height > img_width:
+            new_width = img_width*(max_size/img_height)
+            new_height = max_size
+            resized_img = cv2.resize(img,(int(new_width), int(new_height)))
+            return resized_img
+
+        elif img_height <= img_width:
+            new_width = img_height*(max_size/img_width)
+            new_height = max_size
+            resized_img = cv2.resize(img,(int(new_width), int(new_height)))
+            return resized_img
+
+    return img
+
+def get_image_download_link(img, filename, button_text):
+    button_uuid = str(uuid.uuid4()).replace('-', '')
+    button_id = re.sub('\d+', '', button_uuid)
+
+    buffered = BytesIO()
+    img.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+
+    return get_button_html_code(img_str, filename, 'txt', button_id, button_text)
+
+def get_button_html_code(data_str, filename, filetype, button_id, button_txt='Download file'):
+    custom_css = f""" 
+    <style>
+        #{button_id} {{
+            background-color: rgb(255, 255, 255);
+            color: rgb(38, 39, 48);
+            padding: 0.25em 0.38em;
+            position: relative;
+            text-decoration: none;
+            border-radius: 4px;
+            border-width: 1px;
+            border-style: solid;
+            border-color: rgb(230, 234, 241);
+            border-image: initial;
+
+        }} 
+        #{button_id}:hover {{
+            border-color: rgb(246, 51, 102);
+            color: rgb(246, 51, 102);
+        }}
+        #{button_id}:active {{
+            box-shadow: none;
+            background-color: rgb(246, 51, 102);
+            color: white;
+            }}
+    </style> """
+    
+    href =  custom_css + f'<a href="data:file/{filetype};base64,{data_str}" id="{button_id}" download="{filename}">{button_txt}</a>'
+    return href
+
+def display_single_image(uploaded_file, img_size=800):
+    print('Type: ', type(uploaded_file))
+    st_title_message.markdown("**Processing your image, please wait** ⌛")
+    img_name = uploaded_file.name
+
+    # Open the image
+    pil_img = PIL.Image.open(uploaded_file)
+    img_rgb = np.array(pil_img)
+    resized_img_rgb = resize_img(img_rgb, img_size)
+    resized_pil_img = PIL.Image.fromarray(resized_img_rgb)
+
+    # Send the image to the model
+    output_pil_img = colorizer.plot_transformed_pil_image(resized_pil_img, render_factor=35, compare=False)
+
+    # Plot images
+    st_input_img.image(resized_pil_img, 'Input image', use_column_width=True)
+    st_output_img.image(output_pil_img, 'Output image', use_column_width=True)
+
+    # Show download button
+    st_download_button.markdown(get_image_download_link(output_pil_img, img_name, 'Download Image'), unsafe_allow_html=True)
+
+    # Reset the message
+    st_title_message.markdown("**To begin, please upload an image** 👇")
+
+def process_multiple_images(uploaded_files, img_size=800):
+    num_imgs = len(uploaded_files)
+
+    output_images_list = []
+    img_names_list = []
+    idx = 1
+    for idx, uploaded_file in enumerate(uploaded_files, start=1):
+        st_title_message.markdown("**Processing image {}/{}. Please wait** ⌛".format(idx,
+                                                                                    num_imgs))
+
+        img_name = uploaded_file.name
+        img_type = uploaded_file.type
+
+        # Open the image
+        pil_img = PIL.Image.open(uploaded_file)
+        img_rgb = np.array(pil_img)
+        resized_img_rgb = resize_img(img_rgb, img_size)
+        resized_pil_img = PIL.Image.fromarray(resized_img_rgb)
+
+        # Send the image to the model
+        output_pil_img = colorizer.plot_transformed_pil_image(resized_pil_img, render_factor=35, compare=False)
+
+        output_images_list.append(output_pil_img)
+        img_names_list.append(img_name.split('.')[0])
+
+    # Zip output files
+    zip_path = 'processed_images.zip'
+    zip_buf = zip_multiple_images(output_images_list, img_names_list, zip_path)
+
+    st_download_button.download_button(
+        label='Download ZIP file',
+        data=zip_buf.read(),
+        file_name=zip_path,
+        mime="application/zip"
+    )
+
+    # Show message
+    st_title_message.markdown("**Images are ready for download** 💾")
+
+def zip_multiple_images(pil_images_list, img_names_list, dest_path):
+    # Create zip file on memory
+    zip_buf = BytesIO()
+
+    with ZipFile(zip_buf, 'w', ZIP_DEFLATED) as zipObj:
+        for pil_img, img_name in zip(pil_images_list, img_names_list):
+            with BytesIO() as output:
+                # Save image in memory
+                pil_img.save(output, format="PNG")
+                
+                # Read data
+                contents = output.getvalue()
+
+                # Write it to zip file
+                zipObj.writestr(img_name+".png", contents)
+    zip_buf.seek(0)
+    return zip_buf
+
+
+
+###########################
+###### STREAMLIT CODE #####
+###########################
+
+# General configuration
+# st.set_page_config(layout="centered")
+st.set_page_config(layout="wide")
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.markdown('''
+<style>
+    .uploadedFile {display: none}
+<style>''',
+unsafe_allow_html=True)
+
+# Main window configuration
+st.title("Black and white colorizer")
+st.markdown("This app puts color into your black and white pictures")
+st_title_message = st.empty()
+st_file_uploader = st.empty()
+st_input_img = st.empty()
+st_output_img = st.empty()
+st_download_button = st.empty()
+
+st_title_message.markdown("**Model loading, please wait** ⌛")
+
+# # Sidebar
+st_color_option = st.sidebar.selectbox('Select colorizer mode',
+                                    ('Artistic', 'Stable'))
+                                    
+# st.sidebar.title('Model parameters')
+# det_conf_thres = st.sidebar.slider("Detector confidence threshold", 0.1, 0.9, value=0.5, step=0.1)
+# det_nms_thres = st.sidebar.slider("Non-maximum supression IoU", 0.1, 0.9, value=0.4, step=0.1)
+
+# Load models
+try:
+    print('before loading the model')
+    colorizer = load_model(model_folder, st_color_option)
+    print('after loading the model')
+
+except Exception as e: 
+    colorizer = None
+    print('Error while loading the model. Please refresh the page')
+    print(e)
+    st_title_message.markdown("**Error while loading the model. Please refresh the page**")
+
+if colorizer is not None:
+    st_title_message.markdown("**To begin, please upload an image** 👇")
+
+    #Choose your own image
+    uploaded_files = st_file_uploader.file_uploader("Upload a black and white photo", 
+                                                    type=['png', 'jpg', 'jpeg'],
+                                                    accept_multiple_files=True)
+
+    if len(uploaded_files) == 1:
+        display_single_image(uploaded_files[0], max_img_size)
+    elif len(uploaded_files) > 1:
+        process_multiple_images(uploaded_files, max_img_size)
+
+
---
+++ streamlit/deoldify/visualize.py
@@ -0,0 +1,247 @@
+import cv2
+import gc
+import requests
+from io import BytesIO
+import base64
+from scipy import misc
+from PIL import Image
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from typing import Tuple
+
+import torch
+from fastai.core import *
+from fastai.vision import *
+
+from .filters import IFilter, MasterFilter, ColorizerFilter
+from .generators import gen_inference_deep, gen_inference_wide
+
+
+
+# class LoadedModel
+class ModelImageVisualizer:
+    def __init__(self, filter: IFilter, results_dir: str = None):
+        self.filter = filter
+        self.results_dir = None if results_dir is None else Path(results_dir)
+        self.results_dir.mkdir(parents=True, exist_ok=True)
+
+    def _clean_mem(self):
+        torch.cuda.empty_cache()
+        # gc.collect()
+
+    def _open_pil_image(self, path: Path) -> Image:
+        return Image.open(path).convert('RGB')
+
+    def _get_image_from_url(self, url: str) -> Image:
+        response = requests.get(url, timeout=30, headers={'Accept': '*/*;q=0.8'})
+        img = Image.open(BytesIO(response.content)).convert('RGB')
+        return img
+
+    def plot_transformed_image_from_url(
+        self,
+        url: str,
+        path: str = 'test_images/image.png',
+        results_dir:Path = None,
+        figsize: Tuple[int, int] = (20, 20),
+        render_factor: int = None,
+        
+        display_render_factor: bool = False,
+        compare: bool = False,
+        post_process: bool = True,
+        watermarked: bool = True,
+    ) -> Path:
+        img = self._get_image_from_url(url)
+        img.save(path)
+        return self.plot_transformed_image(
+            path=path,
+            results_dir=results_dir,
+            figsize=figsize,
+            render_factor=render_factor,
+            display_render_factor=display_render_factor,
+            compare=compare,
+            post_process = post_process,
+            watermarked=watermarked,
+        )
+
+    def plot_transformed_image(
+        self,
+        path: str,
+        results_dir:Path = None,
+        figsize: Tuple[int, int] = (20, 20),
+        render_factor: int = None,
+        display_render_factor: bool = False,
+        compare: bool = False,
+        post_process: bool = True,
+        watermarked: bool = True,
+    ) -> Path:
+        path = Path(path)
+        if results_dir is None:
+            results_dir = Path(self.results_dir)
+        result = self.get_transformed_image(
+            path, render_factor, post_process=post_process,watermarked=watermarked
+        )
+        orig = self._open_pil_image(path)
+        if compare:
+            self._plot_comparison(
+                figsize, render_factor, display_render_factor, orig, result
+            )
+        else:
+            self._plot_solo(figsize, render_factor, display_render_factor, result)
+
+        orig.close()
+        result_path = self._save_result_image(path, result, results_dir=results_dir)
+        result.close()
+        return result_path
+
+    def plot_transformed_pil_image(
+        self,
+        input_image: Image,
+        figsize: Tuple[int, int] = (20, 20),
+        render_factor: int = None,
+        display_render_factor: bool = False,
+        compare: bool = False,
+        post_process: bool = True,
+    ) -> Image:
+
+        result = self.get_transformed_pil_image(
+            input_image, render_factor, post_process=post_process
+        )
+
+        if compare:
+            self._plot_comparison(
+                figsize, render_factor, display_render_factor, input_image, result
+            )
+        else:
+            self._plot_solo(figsize, render_factor, display_render_factor, result)
+
+        return result
+
+    def _plot_comparison(
+        self,
+        figsize: Tuple[int, int],
+        render_factor: int,
+        display_render_factor: bool,
+        orig: Image,
+        result: Image,
+    ):
+        fig, axes = plt.subplots(1, 2, figsize=figsize)
+        self._plot_image(
+            orig,
+            axes=axes[0],
+            figsize=figsize,
+            render_factor=render_factor,
+            display_render_factor=False,
+        )
+        self._plot_image(
+            result,
+            axes=axes[1],
+            figsize=figsize,
+            render_factor=render_factor,
+            display_render_factor=display_render_factor,
+        )
+
+    def _plot_solo(
+        self,
+        figsize: Tuple[int, int],
+        render_factor: int,
+        display_render_factor: bool,
+        result: Image,
+    ):
+        fig, axes = plt.subplots(1, 1, figsize=figsize)
+        self._plot_image(
+            result,
+            axes=axes,
+            figsize=figsize,
+            render_factor=render_factor,
+            display_render_factor=display_render_factor,
+        )
+
+    def _save_result_image(self, source_path: Path, image: Image, results_dir = None) -> Path:
+        if results_dir is None:
+            results_dir = Path(self.results_dir)
+        result_path = results_dir / source_path.name
+        image.save(result_path)
+        return result_path
+
+    def get_transformed_image(
+        self, path: Path, render_factor: int = None, post_process: bool = True,
+        watermarked: bool = True,
+    ) -> Image:
+        self._clean_mem()
+        orig_image = self._open_pil_image(path)
+        filtered_image = self.filter.filter(
+            orig_image, orig_image, render_factor=render_factor,post_process=post_process
+        )
+
+        return filtered_image
+
+    def get_transformed_pil_image(
+        self, input_image: Image, render_factor: int = None, post_process: bool = True,
+    ) -> Image:
+        self._clean_mem()
+        filtered_image = self.filter.filter(
+            input_image, input_image, render_factor=render_factor,post_process=post_process
+        )
+
+        return filtered_image
+
+    def _plot_image(
+        self,
+        image: Image,
+        render_factor: int,
+        axes: Axes = None,
+        figsize=(20, 20),
+        display_render_factor = False,
+    ):
+        if axes is None:
+            _, axes = plt.subplots(figsize=figsize)
+        axes.imshow(np.asarray(image) / 255)
+        axes.axis('off')
+        if render_factor is not None and display_render_factor:
+            plt.text(
+                10,
+                10,
+                'render_factor: ' + str(render_factor),
+                color='white',
+                backgroundcolor='black',
+            )
+
+    def _get_num_rows_columns(self, num_images: int, max_columns: int) -> Tuple[int, int]:
+        columns = min(num_images, max_columns)
+        rows = num_images // columns
+        rows = rows if rows * columns == num_images else rows + 1
+        return rows, columns
+
+
+def get_image_colorizer(
+    root_folder: Path = Path('./'), render_factor: int = 35, artistic: bool = True
+) -> ModelImageVisualizer:
+    if artistic:
+        return get_artistic_image_colorizer(root_folder=root_folder, render_factor=render_factor)
+    else:
+        return get_stable_image_colorizer(root_folder=root_folder, render_factor=render_factor)
+
+
+def get_stable_image_colorizer(
+    root_folder: Path = Path('./'),
+    weights_name: str = 'ColorizeStable_gen',
+    results_dir='result_images',
+    render_factor: int = 35
+) -> ModelImageVisualizer:
+    learn = gen_inference_wide(root_folder=root_folder, weights_name=weights_name)
+    filtr = MasterFilter([ColorizerFilter(learn=learn)], render_factor=render_factor)
+    vis = ModelImageVisualizer(filtr, results_dir=results_dir)
+    return vis
+
+
+def get_artistic_image_colorizer(
+    root_folder: Path = Path('./'),
+    weights_name: str = 'ColorizeArtistic_gen',
+    results_dir='result_images',
+    render_factor: int = 35
+) -> ModelImageVisualizer:
+    learn = gen_inference_deep(root_folder=root_folder, weights_name=weights_name)
+    filtr = MasterFilter([ColorizerFilter(learn=learn)], render_factor=render_factor)
+    vis = ModelImageVisualizer(filtr, results_dir=results_dir)
+    return vis
